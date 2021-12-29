ad78f838790c8a891067f3d9c18b813bb5daccff
safe tech <mdsumonaliceng@gmail.com>
2021-12-29 08:31:14 +0600

    4

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqyQBtxfo_na5Da7mJYVfq129QrIx6AxQ",
+  authDomain: "car-mechanic-ce662.firebaseapp.com",
+  projectId: "car-mechanic-ce662",
+  storageBucket: "car-mechanic-ce662.appspot.com",
+  messagingSenderId: "688820984519",
+  appId: "1:688820984519:web:ff6fd1f3cc2fb7ee3bfce2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
